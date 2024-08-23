2c87652039e77512100e284033b073868b1f3c16
IsaacOche <IsaacOche@techcitadels.com>
2024-08-23 15:03:06 +0100

    added querry for restaurant

---
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQuerryHandler.cs
@@ -0,0 +1,21 @@
+﻿
+
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Restaurants.Application.Restaurants.Dtos;
+using Restaurants.Domain.Repositories;
+
+namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants
+{
+    public class GetAllRestaurantsQuerryHandler(ILogger<GetAllRestaurantsQuerryHandler> logger, IRestaurantsRepository restaurantsRepository) : IRequestHandler<GetAllRestaurantsQuerry, IEnumerable<RestaurantDto>>
+    {
+        public async Task<IEnumerable<RestaurantDto>> Handle(GetAllRestaurantsQuerry request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Getting all restaurants");
+            var restaurants = await restaurantsRepository.GetAllAsync();
+
+            var restaurantsDto = restaurants.Select(RestaurantDto.FromEntity);
+            return restaurantsDto!;
+        }
+    }
+}
