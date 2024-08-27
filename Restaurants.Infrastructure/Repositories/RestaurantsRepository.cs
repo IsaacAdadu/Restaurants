4487413b96893f41da963d1e726f592467c7aded
IsaacOche <IsaacOche@techcitadels.com>
2024-08-27 15:22:01 +0100

    commit

--- Restaurants.Infrastructure/Repositories/RestaurantsRepository.cs
+++ Restaurants.Infrastructure/Repositories/RestaurantsRepository.cs
@@ -15,6 +15,12 @@ namespace Restaurants.Infrastructure.Repositories
             return entity.Id;
         }
 
+        public async Task Delete(Restaurant entity)
+        {
+            dbContext.Remove(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
         public async Task<IEnumerable<Restaurant>> GetAllAsync()
         { 
             var restaurants = await dbContext.Restaurants.ToListAsync();
@@ -26,5 +32,10 @@ namespace Restaurants.Infrastructure.Repositories
             var restaurant = dbContext.Restaurants.FirstOrDefaultAsync(x => x.Id == id);
             return restaurant;
         }
+
+        public Task SaveChanges()
+        
+          =>  dbContext.SaveChangesAsync();
+        
     }
 }
