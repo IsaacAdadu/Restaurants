80842e748a1c0b87191ea79c5ac860370b16eaaa
IsaacOche <IsaacOche@techcitadels.com>
2024-08-27 15:20:18 +0100

    added a delete method and an update method

--- Restaurants/Controllers/RestaurantsController.cs
+++ Restaurants/Controllers/RestaurantsController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Restaurants.Application.Restaurants;
 using Restaurants.Application.Restaurants.Commands.CreateRestaurant;
+using Restaurants.Application.Restaurants.Commands.DeleteRestaurant;
+using Restaurants.Application.Restaurants.Commands.UpdateRestaurant;
 using Restaurants.Application.Restaurants.Dtos;
 using Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
 using Restaurants.Application.Restaurants.Queries.GetRestaurantById;
@@ -26,6 +28,24 @@ namespace Restaurants.API.Controllers
             if (restaurant == null)
                 return NotFound();
             return Ok(restaurant);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRestaurant([FromRoute] int id)
+        {
+            var isDeleted = await mediator.Send(new DeleteRestaurantCommand(id));
+            if (isDeleted)
+                return NoContent();
+            return NotFound();
+        }
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> UpdateRestaurant([FromRoute] int id, UpdateRestaurantCommand command)
+        {
+            command.Id = id;
+            var isUpdated = await mediator.Send( command);
+            if (isUpdated)
+                return NoContent();
+            return NotFound();
         }
         [HttpPost]
         public async Task<IActionResult> CreateRestaurant( CreateRestaurantCommand command)
