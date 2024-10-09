eb0664d56643dbe29c740fdcbfa51b147c1fd6f6
IsaacAdadu <isaacadaduoche@gmail.com>
2024-10-10 00:42:12 +0100

    configure Serilog

--- Restaurants/Program.cs
+++ Restaurants/Program.cs
@@ -1,6 +1,8 @@
 using Restaurants.Infrastructure.Extentions;
 using Restaurants.Infrastructure.Seeders;
 using Restaurants.Application.Extentions;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,13 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddInfrastruture(builder.Configuration);
 builder.Services.AddApplication();
+builder.Host.UseSerilog((context, configuration) =>
+
+    configuration
+    .MinimumLevel.Override("Microsoft",LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
+    .WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}]|{SourseContext}| {NewLine}{Message:lj}{NewLine}{Exception}")
+);
 
 var app = builder.Build();
 
