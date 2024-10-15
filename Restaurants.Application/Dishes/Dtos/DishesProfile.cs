fbebbba47a97d875c6de7791a01b4f22dadaff40
IsaacAdadu <isaacadaduoche@gmail.com>
2024-10-15 12:33:32 +0100

    I Map Dish to Restaurant

--- Restaurants.Application/Dishes/Dtos/DishesProfile.cs
+++ Restaurants.Application/Dishes/Dtos/DishesProfile.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using AutoMapper;
+using Restaurants.Application.Dishes.Commands.CreateDish;
+using Restaurants.Domain.Entities;
 
 namespace Restaurants.Application.Dishes.Dtos
 {
-    internal class DishesProfile
+    public class DishesProfile : Profile
     {
+        protected DishesProfile()
+        {
+            CreateMap<CreateDishCommand, Dish>();
+            CreateMap<Dish, DishDto>();
+        }
     }
 }
