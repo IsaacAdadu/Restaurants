8d7b20f3597fafac74daf6ca49ff91216c638c46
IsaacAdadu <isaacadaduoche@gmail.com>
2024-10-15 15:17:52 +0100

    commit

--- Restaurants.Domain/Repositories/IDishesRepository.cs
+++ Restaurants.Domain/Repositories/IDishesRepository.cs
@@ -6,5 +6,6 @@ namespace Restaurants.Domain.Repositories
     public interface  IDishesRepository
     {
         Task<int> Create(Dish entity);
+        Task Delete(IEnumerable<Dish> entities);
     }
 }
