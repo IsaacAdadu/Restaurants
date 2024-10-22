6568c13ef2e6a4a22d754c0d6b7e711d931f0d0a
IsaacAdadu <isaacadaduoche@gmail.com>
2024-10-22 15:09:03 +0100

    Created a presentation layer

---
+++ Restaurants/Extentions/WebApplicationBuilderExtentions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.OpenApi.Models;
+using Restaurants.API.Middlewares;
+using Serilog;
+
+namespace Restaurants.API.Extentions
+{
+    public static class WebApplicationBuilderExtentions
+    {
+        public static void AddPresentation(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+               Reference = new OpenApiReference{ Type = ReferenceType.SecurityScheme, Id = "bearerAuth"}
+            },
+            []
+        }
+
+                });
+            });
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddScoped<ErrorHandlingMiddleware>();
+            builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
+
+            builder.Host.UseSerilog((context, configuration) =>
+
+             configuration.ReadFrom.Configuration(context.Configuration)
+
+            );
+        }
+    }
+}
