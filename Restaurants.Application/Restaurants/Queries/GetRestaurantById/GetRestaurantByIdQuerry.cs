ff62802e9150f78a3df34d5f5772bae0ec900479
IsaacAdadu <isaacadaduoche@gmail.com>
2024-11-11 10:13:02 +0100

    commit

--- Restaurants.Application/Restaurants/Queries/GetRestaurantById/GetRestaurantByIdQuerry.cs
+++ Restaurants.Application/Restaurants/Queries/GetRestaurantById/GetRestaurantByIdQuerry.cs
@@ -2,12 +2,13 @@
 
 using MediatR;
 using Restaurants.Application.Restaurants.Dtos;
+using System.Diagnostics.Contracts;
 
 namespace Restaurants.Application.Restaurants.Queries.GetRestaurantById
 {
     public class GetRestaurantByIdQuerry(int id): IRequest<RestaurantDto>
     {
-
         public int Id { get; } = id;
+       
     }
 }
