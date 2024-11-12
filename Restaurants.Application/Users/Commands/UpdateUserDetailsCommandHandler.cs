7e624e27a1d44745f1909a96a68bf27e55da9a86
IsaacAdadu <isaacadaduoche@gmail.com>
2024-11-12 23:02:32 +0100

    rename the space from User to Users

---
+++ Restaurants.Application/Users/Commands/UpdateUserDetailsCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Application.Users.Commands
+{
+    public class UpdateUserDetailsCommandHandler(ILogger<UpdateUserDetailsCommandHandler> logger, IUserContext userContext, IUserStore<User> userStore) : IRequestHandler<UpdateUserDetailsCommand>
+    {
+        public async Task Handle(UpdateUserDetailsCommand request, CancellationToken cancellationToken)
+        {
+            var user = userContext.GetCurrentUser();
+            logger.LogInformation("Updating user: {UserId}, with {@Request}", user!.Id, request);
+
+            var dbUser = await userStore.FindByIdAsync(user!.Id, cancellationToken);
+
+        }
+    }
+}
