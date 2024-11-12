db9399cee3049997d5c4c41269005babcfe8e6a1
IsaacAdadu <isaacadaduoche@gmail.com>
2024-11-12 23:01:22 +0100

    Added new controller for user identity

---
+++ Restaurants/Controllers/IdentityController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurants.API.Controllers
+{
+    [ApiController]
+    [Route("api/identity")]
+    public class IdentityController
+    {
+        [HttpPatch("user")]
+        public async <IActionResult> UpdateUserDetails()
+        {
+
+        }
+
+    }
+}
