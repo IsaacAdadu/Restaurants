c2ee5e6de250f1c663862c3402298593a3ac40b0
IsaacAdadu <isaacadaduoche@gmail.com>
2024-11-19 13:37:15 +0100

    commit

--- Restaurants/Controllers/IdentityController.cs
+++ Restaurants/Controllers/IdentityController.cs
@@ -1,15 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Restaurants.Application.Users.Commands;
 
 namespace Restaurants.API.Controllers
 {
     [ApiController]
     [Route("api/identity")]
-    public class IdentityController
+    public class IdentityController(IMediator mediator): ControllerBase
     {
         [HttpPatch("user")]
-        public async <IActionResult> UpdateUserDetails()
+        [Authorize]
+        public async Task<IActionResult> UpdateUserDetails (UpdateUserDetailsCommand command )
         {
-
+            await mediator.Send(command);
+            return NoContent();
         }
 
     }
