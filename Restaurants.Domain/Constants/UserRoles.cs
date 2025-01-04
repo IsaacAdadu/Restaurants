e08fb035bb8f3c589e58c4b013c87295989b7813
IsaacAdadu <isaacadaduoche@gmail.com>
2025-01-04 16:17:00 +0100

    Created a UserRole class

---
+++ Restaurants.Domain/Constants/UserRoles.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System.Reflection.Metadata;
+
+namespace Restaurants.Domain.Constants
+{
+    public class UserRoles
+    {
+        public const string User = "User";
+        public const string Owner = "Owner";
+        public const string Admin = "Admin";
+    }
+}
